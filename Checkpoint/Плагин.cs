5b3fa52bd6bf29aca225395de56279dbbe1564c3
russalexei38864 <codyiscoder@gmail.com>
2025-02-05 15:57:14 -0800

    Update Плагин.cs

--- Checkpoint/Плагин.cs
+++ Checkpoint/Плагин.cs
@@ -1,4 +1,4 @@
-﻿using BepInEx;
+using BepInEx;
 using UnityEngine;
 
 namespace Checkpoint
@@ -7,60 +7,78 @@ namespace Checkpoint
     public class Плагин : BaseUnityPlugin
     {
         public static bool ВМоде => NetworkSystem.Instance.InRoom && NetworkSystem.Instance.GameModeString.Contains("MODDED");
-        private static bool правый_индекс, левый_индекс, телепорт_использован, Инициализировано;
+        private static bool правый_индекс, левый_индекс, телепорт_использован, инициализировано, включено;
         private static GameObject чекпоинт;
 
         void Start() => GorillaTagger.OnPlayerSpawned(Инициализация);
 
-        private void Инициализация()
+        void OnEnable()
         {
+            включено = true;
+            чекпоинт?.SetActive(true);
+        }
+
+        void OnDisable()
+        {
+            включено = false;
+            чекпоинт?.SetActive(false);
+        }
+
+        void Инициализация()
+        {
+            if (инициализировано) return;
+
             чекпоинт = GameObject.CreatePrimitive(PrimitiveType.Sphere);
             чекпоинт.transform.position = new Vector3(-66.8761f, 11.8781f, -82.3425f);
             чекпоинт.transform.localScale = Vector3.one * 0.2f;
             чекпоинт.name = "Чекпоинт";
             Destroy(чекпоинт.GetComponent<SphereCollider>());
             ИзменитьЦвет(Color.green);
+            чекпоинт.SetActive(false);
 
-            Инициализировано = true;
+            инициализировано = true;
         }
 
         void Update()
         {
-            if (ВМоде && Инициализировано)
+            if (!ВМоде || !инициализировано || !включено)
             {
-                левый_индекс = ControllerInputPoller.instance.leftControllerIndexFloat > 0.5f;
-                правый_индекс = ControllerInputPoller.instance.rightControllerIndexFloat > 0.5f;
-                чекпоинт.SetActive(false);
-                чекпоинт.SetActive(true);
-
-                if (левый_индекс && !правый_индекс)
-                {
-                    чекпоинт.transform.SetParent(GorillaTagger.Instance.leftHandTransform);
-                    чекпоинт.transform.localPosition = Vector3.zero;
-                }
-                else if (!левый_индекс)
-                {
-                    чекпоинт.transform.SetParent(null);
-                }
-
-                if (правый_индекс && !левый_индекс && !телепорт_использован)
-                {
-                    GorillaLocomotion.Player.Instance.TeleportTo(чекпоинт.transform);
-                    телепорт_использован = true;
-                    ИзменитьЦвет(Color.red);
-                }
-                else if (!правый_индекс)
-                {
-                    телепорт_использован = false;
-                    ИзменитьЦвет(Color.green);
-                }
-
-                чекпоинт.transform.rotation = GorillaLocomotion.Player.Instance.headCollider.transform.rotation;
+                чекпоинт?.SetActive(false);
+                return;
             }
-            else
+
+            ОбновитьСостояние();
+        }
+
+        private void ОбновитьСостояние()
+        {
+            левый_индекс = ControllerInputPoller.instance.leftControllerIndexFloat > 0.5f;
+            правый_индекс = ControllerInputPoller.instance.rightControllerIndexFloat > 0.5f;
+            чекпоинт.SetActive(true);
+
+            if (левый_индекс && !правый_индекс)
             {
-                чекпоинт.SetActive(false);
+                чекпоинт.transform.SetParent(GorillaTagger.Instance.leftHandTransform, false);
+                чекпоинт.transform.localPosition = Vector3.zero;
             }
+            else if (!левый_индекс)
+            {
+                чекпоинт.transform.SetParent(null);
+            }
+
+            if (правый_индекс && !левый_индекс && !телепорт_использован)
+            {
+                GorillaLocomotion.Player.Instance.TeleportTo(чекпоинт.transform);
+                телепорт_использован = true;
+                ИзменитьЦвет(Color.red);
+            }
+            else if (!правый_индекс)
+            {
+                телепорт_использован = false;
+                ИзменитьЦвет(Color.green);
+            }
+
+            чекпоинт.transform.rotation = GorillaLocomotion.Player.Instance.headCollider.transform.rotation;
         }
 
         private static void ИзменитьЦвет(Color цвет)
