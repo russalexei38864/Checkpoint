e34898d4b78102026cae55adeb8c6d83a89dc235
russalexei38864 <codyiscoder@gmail.com>
2025-02-05 15:57:51 -0800

    update version

--- Checkpoint/Константы.cs
+++ Checkpoint/Константы.cs
@@ -4,6 +4,6 @@
     {
         public const string GUID = "com.russalexei_38864.gorillatag.checkpoint";
         public const string Name = "Checkpoint";
-        public const string Version = "1.0.0";
+        public const string Version = "1.1.0";
     }
 }
